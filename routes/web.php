d83b7585728bb6418431687392ed654b4bcfe683
NatanaelWT <natanaelhdxd25@gmail.com>
2025-05-16 14:11:35 +0700

    update daftar siswa

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiswaController;
 
-Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa');
+Route::get('/siswa', [SiswaController::class, 'index'])->name('siswa.index');
+Route::get('/siswa/create', [SiswaController::class, 'create'])->name('siswa.create');
+Route::post('/siswa', [SiswaController::class, 'store'])->name('siswa.store');
+
 
 Route::get('/', function () {
     return view('dashboard');
