18f44c49c7af31e3b9c0dc511798c99eeacf1408
NatanaelWT <natanaelhdxd25@gmail.com>
2025-05-16 15:13:29 +0700

    fix migration

--- database/migrations/2025_04_03_172044_create_siswas_table.php
+++ database/migrations/2025_04_03_172044_create_siswas_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('siswas', function (Blueprint $table) {
             $table->id();
+            $table->integer('nis');
+            $table->integer('tahun');
             $table->string('nama');
+            $table->string('kelas');
             $table->string('alamat')->nullable();
             $table->string('nomor_telepon')->nullable();
             $table->date('tanggal_lahir')->nullable();
